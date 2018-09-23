e17b8f64449861d41e45f5209666ebf03e1383a3
Sander Grindheim <sandsalamand@gmail.com>
2018-09-23 00:00:41 -0700

    about to make another big change, use malloced buffer instead of static length buf

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,10 @@
 /*   By: sgrindhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/15 23:48:23 by sgrindhe          #+#    #+#             */
-/*   Updated: 2018/09/19 01:41:50 by sgrindhe         ###   ########.fr       */
+/*   Updated: 2018/09/23 00:00:17 by sgrindhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "get_next_line.h"
 #define PREV_LINES BUFF_SIZE * line_ct
 
@@ -17,22 +18,18 @@ int		getstr(int strt, int end, char buf[BUFF_SIZE], char **line)
 	int i;
 	char *str;
 
-	ft_putstr("start: ");
-	putnbr_endl(strt);
-	ft_putstr("end: ");
-	putnbr_endl(end);
-	str = malloc(sizeof(char) * (end - strt));
+	str = ft_strnew(end - strt - 1);
 	i = 0;
 	while (strt <= end)
 	{
-		//ft_putchar(buf[strt]);
 		str[i] = buf[strt];
 		i++;
 		strt++;
 	}
 	if (!(*line))
 		*line = str;	
-	*line = ft_strjoin(*line, str);
+	else
+		*line = ft_strjoin(*line, str);
 	free(str);
 	return (0);
 }
@@ -42,33 +39,37 @@ int		get_next_line(const int fd, char **line)
 	static char		act_buf[BUFF_SIZE];
 	static int		cur_byte = 0;
 	static int		line_ct = 0;
+	char			*buf;
 	int				strt_byte;
 
+	free(*line);
+	*line = ft_strnew(1);
 	if (cur_byte == 0)
 	{
-		free(*line);
 		if (read(fd, &act_buf, BUFF_SIZE) == 0)
 			return (0);
 	}
 	strt_byte = cur_byte;
+	buf = ft_strnew((BUFF_SIZE * (line_ct + 1)) - 1);
 	while (act_buf[cur_byte - (PREV_LINES)])
 	{
-		if (act_buf[cur_byte - (PREV_LINES)] == '\n' ||
-			 act_buf[cur_byte - (PREV_LINES)] == 26)
+		buf[cur_byte - (PREV_LINES)] = act_buf[cur_byte - (PREV_LINES)];
+		if (act_buf[cur_byte - (PREV_LINES)] == '\n')
 		{
-			getstr(strt_byte, cur_byte - (PREV_LINES), act_buf, &(*line));
+			getstr(strt_byte, cur_byte - (PREV_LINES) - 1, act_buf, &(*line));
 			cur_byte++;
-			line_ct++;
 			break;
 		}
-		cur_byte++;
-		if (cur_byte - (PREV_LINES) == BUFF_SIZE)
+		if (cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
 		{
+			*line = ft_strjoin(*line, buf);
 			getstr(strt_byte, BUFF_SIZE - 1, act_buf, &(*line));
+			line_ct++;
+			cur_byte++;
 			if (read(fd, &act_buf, BUFF_SIZE) == 0)
 				return (0);
-			line_ct++;
 		}
+		cur_byte++;
 	}
 	return (0);
 }
