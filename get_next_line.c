d2d092358c3d9ee3b8e63899ea96ec819f7a474d
Sander Grindheim <sgrindhe@student.42.us.org>
2018-09-23 19:25:57 -0700

    switched to using static struct, going to split off main loop into separate function for norminette

--- get_next_line.c
+++ get_next_line.c
@@ -11,44 +11,42 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define PREV_LINES BUFF_SIZE * line_ct
+#define PREV_LINES BUFF_SIZE * v.line_ct
 
 int		get_next_line(const int fd, char **line)
 {
-	static char		act_buf[BUFF_SIZE];
-	static int		cur_byte = 0;
-	static int		line_ct = 0;
+	static t_vars	v = {0, 0, {}};
 	char			*buf;
 	int				cntr;
 
 	free(*line);
 	*line = ft_strnew(1);
-	if (cur_byte == 0)
+	if (v.cur_byte == 0)
 	{
-		if (read(fd, &act_buf, BUFF_SIZE) < 0)
+		if (read(fd, &v.act_buf, BUFF_SIZE) < 0)
 			return (-1);
 	}
-	buf = ft_strnew((BUFF_SIZE * (line_ct + 1)) - 1);
+	buf = ft_strnew((BUFF_SIZE * (v.line_ct + 1)) - 1);
 	cntr = 0;
-	while (act_buf[cur_byte - (PREV_LINES)])
+	while (v.act_buf[v.cur_byte - (PREV_LINES)])
 	{
-		buf[cntr] = act_buf[cur_byte - (PREV_LINES)];
-		if (act_buf[cur_byte - (PREV_LINES)] == '\n')
+		buf[cntr] = v.act_buf[v.cur_byte - (PREV_LINES)];
+		if (v.act_buf[v.cur_byte - (PREV_LINES)] == '\n')
 		{
 			buf[cntr] = '\0';
 			*line = ft_strjoin(*line, buf);
 			free(buf);
-			cur_byte++;
+			v.cur_byte++;
 			return(1);
 		}
-		if (cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
+		if (v.cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
 		{
 			*line = ft_strjoin(*line, buf);
 			ft_strclr(buf);
-			line_ct++;
+			v.line_ct++;
 			cntr = 0;
-			cur_byte++;
-			if (read(fd, &act_buf, BUFF_SIZE) == 0)
+			v.cur_byte++;
+			if (read(fd, &(v.act_buf), BUFF_SIZE) == 0)
 			{
 				free(buf);
 				return (0);
@@ -57,7 +55,7 @@ int		get_next_line(const int fd, char **line)
 		else
 		{
 			cntr++;
-			cur_byte++;
+			v.cur_byte++;
 		}
 	}
 	return (0);
