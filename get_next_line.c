91a4f39b123333f7f51d44ec0ade9359974a335e
Sander Grindheim <sgrindhe@student.42.us.org>
2018-09-24 02:30:06 -0700

    okay.

--- get_next_line.c
+++ get_next_line.c
@@ -1,13 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgrindhe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/15 23:48:23 by sgrindhe          #+#    #+#             */
+/*   Updated: 2018/09/23 00:41:45 by sgrindhe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #define PREV_LINES BUFF_SIZE * v.line_ct
 
+int		check_buf(t_vars v, char **buf, int fd, char **line)
+{
+	(*buf)[v.ctr] = v.act_buf[v.cur_byte - (PREV_LINES)];
+	if (v.act_buf[v.cur_byte - (PREV_LINES)] == '\n')
+	{
+		(*buf)[v.ctr] = '\0';
+		*line = ft_strjoin(*line, *buf);
+		free(*buf);
+		v.cur_byte++;
+		return (1);
+	}
+	if (v.cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
+	{
+		*line = ft_strjoin(*line, *buf);
+		ft_strclr(*buf);
+		v.line_ct++;
+		v.ctr = 0;
+		v.cur_byte++;
+		if (read(fd, &(v.act_buf), BUFF_SIZE) == 0)
+		{
+			free(*buf);
+			return (0);
+		}
+	}
+	return (2);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	static t_vars	v = {0, 0, {}};
-	char			*buf;
-	int				cntr;
+	static t_vars	v = {0, 0, {}, 0};
+	char			buf[BUFF_SIZE + 1];
+	int				output;	
 
-	free(*line);
 	*line = ft_strnew(1);
 	if (v.cur_byte == 0)
 	{
@@ -15,34 +53,19 @@ int		get_next_line(const int fd, char **line)
 			return (-1);
 	}
 	buf = ft_strnew((BUFF_SIZE * (v.line_ct + 1)) - 1);
-	cntr = 0;
+	putnbr_endl((BUFF_SIZE * (v.line_ct + 1)) - 1);
+	v.ctr = 0;
 	while (v.act_buf[v.cur_byte - (PREV_LINES)])
 	{
-		buf[cntr] = v.act_buf[v.cur_byte - (PREV_LINES)];
-		if (v.act_buf[v.cur_byte - (PREV_LINES)] == '\n')
+		if ((output = check_buf(v, &buf, fd, line)) == 0)
+			return (0);
+		else if (output == 1)
 		{
-			buf[cntr] = '\0';
-			*line = ft_strjoin(*line, buf);
-			free(buf);
-			v.cur_byte++;
-			return(1);
-		}
-		if (v.cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
-		{
-			*line = ft_strjoin(*line, buf);
-			ft_strclr(buf);
-			v.line_ct++;
-			cntr = 0;
-			v.cur_byte++;
-			if (read(fd, &(v.act_buf), BUFF_SIZE) == 0)
-			{
-				free(buf);
-				return (0);
-			}
+			return (1);
 		}
 		else
 		{
-			cntr++;
+			v.ctr++;
 			v.cur_byte++;
 		}
 	}
