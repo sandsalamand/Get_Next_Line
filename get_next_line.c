9808340717f7550e3d5b53e1569e35bddd2977c3
Sander Grindheim <sandsalamand@gmail.com>
2018-09-26 03:25:57 -0700

    realized I needed to be using pointers to change original static struct values, contrary to what I interpteted from the stack overflow answer I read :|

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,32 @@
 /*   By: sgrindhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/15 23:48:23 by sgrindhe          #+#    #+#             */
-/*   Updated: 2018/09/23 00:41:45 by sgrindhe         ###   ########.fr       */
+/*   Updated: 2018/09/26 03:23:42 by sgrindhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define PREV_LINES BUFF_SIZE * v.line_ct
+#define PREV_LINES BUFF_SIZE * (*v).line_ct
 
-int		check_buf(t_vars v, char **buf, int fd, char **line)
+int		check_buf(t_vars *v, char **buf, int fd, char **line)
 {
-	(*buf)[v.ctr] = v.act_buf[v.cur_byte - (PREV_LINES)];
-	if (v.act_buf[v.cur_byte - (PREV_LINES)] == '\n')
+	(*buf)[(*v).ctr] = (*v).act_buf[(*v).cur_byte - (PREV_LINES)];
+	if ((*v).act_buf[(*v).cur_byte - (PREV_LINES)] == '\n')
 	{
-		(*buf)[v.ctr] = '\0';
+		(*buf)[(*v).ctr] = '\0';
 		*line = ft_strjoin(*line, *buf);
 		free(*buf);
-		v.cur_byte++;
+		(*v).cur_byte++;
 		return (1);
 	}
-	if (v.cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
+	if ((*v).cur_byte - (PREV_LINES) >= BUFF_SIZE - 1)
 	{
 		*line = ft_strjoin(*line, *buf);
 		ft_strclr(*buf);
-		v.line_ct++;
-		v.ctr = 0;
-		v.cur_byte++;
-		if (read(fd, &(v.act_buf), BUFF_SIZE) == 0)
+		(*v).line_ct++;
+		(*v).ctr = 0;
+		(*v).cur_byte++;
+		if (read(fd, &((*v).act_buf), BUFF_SIZE) == 0)
 		{
 			free(*buf);
 			return (0);
@@ -42,30 +42,31 @@ int		check_buf(t_vars v, char **buf, int fd, char **line)
 
 int		get_next_line(const int fd, char **line)
 {
-	static t_vars	v = {0, 0, {}, 0};
+	static t_vars	vars = {0, 0, {}, 0};
+	t_vars			*v;
 	char			*buf;
 	int				output;	
 
-	*line = ft_strnew(1);
-	if (v.cur_byte == 0)
+	v = &vars;
+	if ((*v).cur_byte - PREV_LINES == 0)
 	{
-		if (read(fd, &v.act_buf, BUFF_SIZE) < 0)
+		if (!(*line))
+			*line = ft_strnew(1);
+		if (read(fd, &(*v).act_buf, BUFF_SIZE) < 0)
 			return (-1);
 	}
-	buf = ft_strnew((BUFF_SIZE * (v.line_ct + 1)) - 1);
-	v.ctr = 0;
-	while (v.act_buf[v.cur_byte - (PREV_LINES)])
+	buf = ft_strnew((BUFF_SIZE * ((*v).line_ct + 1)) - 1);
+	(*v).ctr = 0;
+	while ((*v).act_buf[(*v).cur_byte - (PREV_LINES)])
 	{
 		if ((output = check_buf(v, &buf, fd, line)) == 0)
 			return (0);
 		else if (output == 1)
-		{
 			return (1);
-		}
 		else
 		{
-			v.ctr++;
-			v.cur_byte++;
+			(*v).ctr++;
+			(*v).cur_byte++;
 		}
 	}
 	return (0);
