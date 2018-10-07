e7d1dc2123660149fc344d59605b595e974b795e
Sander Grindheim <sandsalamand@gmail.com>
2018-10-07 01:23:12 -0700

    multi-line standard input is fucked up, also tons of leaks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sgrindhe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/16 01:47:04 by sgrindhe          #+#    #+#             */
-/*   Updated: 2018/10/06 02:53:10 by sgrindhe         ###   ########.fr       */
+/*   Updated: 2018/10/07 01:20:48 by sgrindhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,27 @@ int main(int argc, char **argv)
 	int		fd;
 	char	*line;
 
-	i = 25;
+	i = 0;
 	if (argc != 2) {
-		ft_putendl("file name needed");
-		return (0);
+		//ft_putendl("file name needed");
+		//return (0);
 	}
 	line = malloc(sizeof(char) * 5);
 	line[0] = 'E';
 	free(line);
 	line = NULL;
 	fd = open(argv[1], O_RDONLY);
-	while (i != 0)
+	while (i <= 5)
 	{
-		i = get_next_line(fd, &line);
-		putnbr_endl(i);
+		putnbr_endl(get_next_line(0, &line));
+		//putnbr_endl(i);
 		//get_next_line(fd, &line);
 		ft_putendl(line);
 		free(line);
+		i++;
 	}
 	close(fd);
+	ft_strdel(&line);
 	return (0);
 }
 
